0ff2eda852f1bfca1c55c63f644fffc5c90aa022
Sunny-Maheshwari <maheshwarisunny17@gmail.com>
2024-07-09 00:39:23 +0500

    Word counter project create

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the words",
+    },
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(`Words count of your sentence is ${words.length}`);
